eebe2c298c6d6113a843e1bcb00b476767b3fab0
gabeldev <gabrieljesus@alunos.utfpr.edu.br>
2025-08-07 10:28:00 -0300

    solver v1

--- solver.py
+++ solver.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     plt.figure(figsize=(12,5))
     plt.subplot(1, 2, 1)
     plt.plot(t, theta)
-    plt.title("Posiçã angular θ(t)")
+    plt.title("Posição angular θ(t)")
     plt.xlabel("Tempo")
     plt.ylabel("θ")
 
